eaecb93ff2cb128edf5bb3e3fd9b12d187caecd9
USER <mochamadanggi100@gmail.com>
2021-05-25 04:46:59 +0700

    First init

--- app/src/main/java/com/mochanggiandrianto/warkopais/MainActivity.kt
+++ app/src/main/java/com/mochanggiandrianto/warkopais/MainActivity.kt
@@ -3,7 +3,7 @@ package com.mochanggiandrianto.warkopais
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {git push -u origin main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
